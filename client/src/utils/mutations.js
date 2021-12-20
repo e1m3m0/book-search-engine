e533d031cd99f36ae4c7668d419608cf902f2384
e1m3m0 <gamateo@gmail.com>
2021-12-19 21:59:32 -0500

    queries and mutations client completed, apollo client and graphql installed

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,59 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook(bookId: String!, title: String!, description: String!, author: String, image: String, link: String) {
+    saveBook(bookId: $bookId, title: $title, description: $description, author: $author, image: $image, link: $link) {
+      _id
+      bookCount
+      savedBooks {
+        bookId
+        author
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook(bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      bookCount
+      savedBooks {
+        bookId
+        author
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`
